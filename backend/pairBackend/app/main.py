45718e74e3089378394a15a39656058f7561d1e1
Gopi.Chandu <Gopi.Chandu@tsworks.io>
2025-12-09 13:21:09 +0530

    updated frontend

--- backend/pairBackend/app/main.py
+++ backend/pairBackend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://pair-programming-phi.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
